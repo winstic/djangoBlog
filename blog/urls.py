5cd92651823dbf82ccf7dc28d5a27c56259da048
winstic <wzc0314@163.com>
2017-07-20 17:24:17 +0800

    added detail blog html and show it with css

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,8 @@ from . import views			#导入全部视图
 urlpatterns = [
 	#只匹配空字符，即若访问127.0.0.1:8000则跳转到post_list这个视图
 	#name 是url的名字，用来唯一标识一个视图
-	url(r'^$', views.post_list, name='post_list')
+	url(r'^$', views.post_list, name='post_list'),
+	#'?P<post_id>'表示会将此处的内容放置在post_id变量中，并传递给视图
+	#'[0-9]'+表示至少一个数字
+	url(r'^post/(?P<post_id>[0-9]+)/$', views.post_detail, name='post_detail')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
@@ -10,3 +10,7 @@ def post_list(request):
 	posts = Post.objects.filter(published_date__lte = timezone.now()).order_by('published_date')
 	#将posts数据集传递到模板文件中
 	return render(request, 'blog/post_list.html', {'posts':posts})
+	
+def post_detail(request, post_id):
+	post = get_object_or_404(Post, pk = post_id)
+	return render(request, 'blog/post_detail.html', {'post':post})
